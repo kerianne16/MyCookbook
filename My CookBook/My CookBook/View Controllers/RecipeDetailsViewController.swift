e19f1178adb31598673185e579c25a1122683179
Keri Levesque <kerianne166@gmail.com>
2020-12-09 21:56:58 -0500

    started storyboard layout + corresponding controller files

---
+++ My CookBook/My CookBook/View Controllers/RecipeDetailsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  RecipeDetailsViewController.swift
+//  My CookBook
+//
+//  Created by Keri Levesque on 12/3/20.
+//  Copyright © 2020 Keri Levesque. All rights reserved.
+//
+
+import UIKit
+
+class RecipeDetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
